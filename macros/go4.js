5736dff23c92c0697e1409b9d3e07a2620d1f64a
francesco <francesco.bracchi@create-net.org>
2014-01-22 17:15:27 +0100

    some little fixes

--- macros/go4.js
+++ macros/go4.js
@@ -15,14 +15,50 @@ var trampoline = function (cont) {
 
 var identity = function (x) { return x; };
 
-var go_run = function (monad) {
-  return trampoline(monad(identity));
+var go_run = function (monad, cnt) {
+  if (! cnt) {
+    cnt = identity;
+  }
+  return trampoline(monad(cnt));
 };
 
+// monad operations
+// bind
+var go_bind = function (m, fn) {
+  return function (cont) {
+    return jump(function () {
+      return m (function (v) {
+        return jump (function () {
+          var n = fn (v);
+          n (cont);
+        });
+      });
+    });
+  };
+};
+
+// return
 var go_return = function (val) {
   return function (cont) {
     return jump (function () {
       return cont (val);
+    });
+  };
+};
+
+// basic operations
+var go_take = function (ch) {
+  return function (cont) {
+    return ch.take (function (v) {
+      trampoline(cont (v));
+    });
+  };
+};
+
+var go_put = function (v, ch) {
+  return function (cont) {
+    return ch.put (v, function () {
+      trampoline(cont (v));
     });
   };
 };
