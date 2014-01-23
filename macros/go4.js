d9d58d1b0b79ae4599980cbf8e7a0b7f43c9c88f
Your Name <frbracch@gmail.com>
2014-01-23 08:33:00 +0100

    nth rewrite

--- macros/go4.js
+++ macros/go4.js
@@ -10,7 +10,7 @@ macro fun {
 // creates a delayed execution, it's used by
 // trampoline to run step by step without 
 // incurring in stack overflows
-macro jump {
+macro go_jump {
     rule {
 	$e
     } => {
@@ -21,7 +21,7 @@ macro jump {
 // simply put the continuation as an extra
 // element in the sequence (surrounded by 
 // round brackets 
-macro with_cont {
+macro with_args {
     rule {
 	($cont) { $e }
     } => {
@@ -31,7 +31,7 @@ macro with_cont {
 
 // reflect is used to access explicitely to the
 // continuation
-macro reflect {
+macro go_reflect {
     rule {
 	( $v:lit ) { $b } ( $cont )
     } => {
@@ -41,12 +41,12 @@ macro reflect {
 
 // reify assigns to a variable a monad, and 
 // uses it in the context
-macro reify {
+macro go_reify {
     rule {
 	$v:lit = $e { $x } ( $cont )
     } => {
 	macro $v { rule { } => { $e } }
-	with_cont ( $cont ) { $x };
+	with_args ( $cont ) { $x };
     }
 }
 
@@ -58,7 +58,7 @@ macro go_fun {
     } => {
 	fun $fp {
 	    fun (cont) {
-		with_cont (cont) { $b }
+		with_args (cont) { $b }
 	    }
 	}
     }
@@ -74,7 +74,7 @@ macro go_macro  {
 	    rule {
 		$patt ... (cont) 
 	    } => {
-		with_cont (cont) { $result ... }
+		with_args (cont) { $result ... }
 	    }
 	}
     }
@@ -82,34 +82,38 @@ macro go_macro  {
 
 go_macro go_run {
     rule {
-	$p
+	($m)
     } => {
-	var identity = fun (i) { i };
-	go.trampoline (with_cont (identity) { $p });
+	go_run ($m, fun (x) { x })
+    }
+    rule {
+	($m, $cont) 
+    } => {
+	go.trampoline (with_args ($cont) { $m })
     }
 }
 
 // monad return operator
-go_macro ret {
+go_macro go_return {
     rule {
-	$v
+	( $v )
     } => {
-	reflect (cont) {
-	    jump ( cont ( $v ) )
+	go_reflect (cont) {
+	    go_jump ( cont ( $v ) )
 	}
     }
 }
 
 // monad bind operator
-go_macro bind {
+go_macro go_bind {
     rule {
 	 $v:lit = $m { $n }
     } => {
-	reify m = $m {
-	    reify n = $n {
-		reflect (cont) {
-		    jump ( m ( fun (v) { 
-			jump ( n (cont) )
+	go_reify m = $m {
+	    go_reify n = $n {
+		go_reflect (cont) {
+		    go_jump ( m ( fun (v) { 
+			go_jump ( n (cont) )
 		    }))
 		}
 	    }
@@ -118,41 +122,45 @@ go_macro bind {
 }
 
 // monad sequence operator
-go_macro seq {
+go_macro go_seq {
     rule {
 	{ $a ; $b }
     } => {
-	bind ignore = $a { $b }
+	go_bind ignore = $a { $b }
     }
 }
 
-go_macro recv {
+go_macro go_recv {
     rule {
 	($ch)
     } => {
-	reflect (cont) { 
-	    ($ch).recv (jump(cont(true)));
+	go_reflect (cont) { 
+	    ($ch).recv (fun (v) {
+		go_run (cont(v)); 
+	    });
 	}
     }
 }
 
-go_macro send {
+go_macro go_send {
     rule {
 	($ch, $v) 
     } => {
-	reflect (cont) {
-	    ($ch).send (jump (cont (true)));
+	go_reflect (cont) {
+	    ($ch).send (fun (v) {
+		go_run (cont (v));
+	    });
 	}
     }
 }
 
 go_macro go_if {
     rule {
-	( $t, $l, $r)
+	( $t ) $l else $r
     } => {
-	reify l = $l {
-	    reify r = $r {
-		reflect (cont) {
+	go_reify l = $l {
+	    go_reify r = $r {
+		go_reflect (cont) {
 		    ( $t ) ? l (cont) : r (cont)
 		}
 	    }
@@ -163,25 +171,64 @@ go_macro go_if {
 
 go_macro go_eval {
     rule {
-	{ recv $v:lit <- $ch; $es ... }
+	block { recv $v:lit <- $ch; $es ... }
     } => {
-	bind $b = recv ($ch) { 
+	go_bind $b = go_recv ($ch) { 
 	    go_eval { $es ... } 
 	}
     }
 
     rule {
-	{ send $m -> $ch; $es ... }
+	block { send $m -> $ch; $es ... }
     } => {
-	bind ign = send ($v, $ch) {
+	go_bind ign = go_send ($v, $ch) {
 	    go_eval { $es ... } 
 	}
     }
+
     rule {
-	{ if ( $t ) $l else $r }
+	block {}
     } => {
-	go_if ($t, go_eval $l, go_eval $r);
+	go_return (undefined)
     }
+    rule {
+	block { $e }
+    } => {
+	go_eval $e;
+    }
+
+    rule {
+	block { $e $es ... }
+    } => {
+	go_seq ( go_eval $e, go_eval block { $es ... } )
+    }
+
+    rule {
+	if ( $t ) { $l ... }
+    } => {
+	go if ( $t ) block { $l ... }
+    }
+    
+    rule {
+	if ( $t ) $l 
+    } => {
+	go if ( $t ) $l else {}
+    }
+
+    rule {
+	if ( $t ) $l else { $r ... }
+    } => {
+	if ( $t ) $l else block { $r ... }
+    }
+    
+    rule {
+	if ( $t ) $l else $r
+    } => {
+	go_if ( $t ) (go $l) else (go $r)
+    }
+
+
+
     // TODO HERE WHILE?
     rule {
 	{ while ( $t ) { $b ... } $e ... }
@@ -189,20 +236,4 @@ go_macro go_eval {
 	go_while 
     }
 
-    rule {
-	{}
-    } => {
-	ret(undefined)
-    }
-    rule {
-	{ $e }
-    } => {
-	ret($e)
-    }
-
-    rule {
-	{ $e $es ... }
-    } => {
-	go_seq ( go_eval $e, go_eval $es)
-    }
 }
