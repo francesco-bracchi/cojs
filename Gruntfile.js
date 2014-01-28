ff6de64778d004f68989cf5beb9232a0cbd43744
Your Name <frbracch@gmail.com>
2014-01-28 07:58:47 +0100

    don't know

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,34 +2,20 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-	pkg: grunt.file.readJSON('package.json'),
-	sweetjs: {
-	    options: {
-		// Task-specific options go here.
-		src: 'src/**/*.js',
-		modules: ['./macros/go.js']
-	    }
-	},
-	uglify: {
-	    options: {
-		banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-	    },
-	    build: {
-		src: 'src/lib.js',
-		dest: 'build/<%= pkg.name %>.min.js'
-	    }
-	},
-	my_src_files: ['src/*.js', 'src/lib/*.js'],
+				pkg: grunt.file.readJSON('package.json'),
+				sweetjs: {
+						options: {
+								modules: ['./macros/go.js']
+						},
+						build: {
+								src: 'src/lib/*.js',
+								dest: 'build/go.js'
+						}
+				}
     });
-
-    // Load the plugin that provides "sweet" task
-    grunt.loadNpmTasks('grunt-sweet.js');
     
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-
-
-    // Default task(s).
+		grunt.loadNpmTasks('grunt-sweet.js');
+    
     grunt.registerTask('default', ['sweetjs']);
 
 };
--- macros/go.js
+++ macros/go.js
@@ -3,144 +3,144 @@ var monad = require ('go/lib/monad.js'),
 
 macro goexpr {
     rule {
-	{
-	    recv $v:ident <- $ch:expr;
-	    $gs ...
-	}
+				{
+						recv $v:ident <- $ch:expr;
+						$gs ...
+				}
     } => {
-	$ch . recv () . bind (function ( $v) { return goexpr { $gs ... } } )
-	// bind ( recv ( $ch ) , function ( $v ) { return goexpr { $gs ... } } )
+				$ch . recv () . bind (function ( $v) { return goexpr { $gs ... } } )
+				// bind ( recv ( $ch ) , function ( $v ) { return goexpr { $gs ... } } )
     }
 
     rule {
-	{
-	    send $m:expr -> $ch:expr;
-	}
+				{
+						send $m:expr -> $ch:expr;
+				}
     } => {
-	// send ( $m , $ch )
-	( $ch ) . send ( $m )
+				// send ( $m , $ch )
+				( $ch ) . send ( $m )
     }
     rule {
-	{
-	    send $m:expr -> $ch:expr;
-	    $gs ...
-	}
+				{
+						send $m:expr -> $ch:expr;
+						$gs ...
+				}
     } => {
-	( $ch ) . send ( $m ) . bind (function ( ) { return goexpr { $gs ... } } );
-    }
-
-    rule {
-	{
-	    while ( $t:expr ) { $b ... }
-	    $gs ...
-	}
-    } => {
-	(function () {
-          var loop = function (_) {
-            var b = goexpr { $b ... };
-            if ( $t ) {
-              return b.bind(loop);
-            }
-	    return goexpr { $gs ... };
-	  };
-	  return loop();
-	}())
+				( $ch ) . send ( $m ) . bind (function ( ) { return goexpr { $gs ... } } );
     }
 
     rule {
-	{
-	    while ( $t:expr ) $e:expr ;
-	    $gs ...
-	}
+				{
+						while ( $t:expr ) { $b ... }
+						$gs ...
+				}
     } => {
-	goexpr { while ( $t ) { $e } $gs ... }
+				(function () {
+						var loop = function (_) {
+								var b = goexpr { $b ... };
+								if ( $t ) {
+										return b.bind(loop);
+								}
+								return goexpr { $gs ... };
+						};
+						return loop();
+				}())
     }
 
     rule {
-	{ throw $e:expr; }
+				{
+						while ( $t:expr ) $e:expr ;
+						$gs ...
+				}
     } => {
-	// monad (cont, fail) { return promise (fail ($e)) }
-	monad . fail ( $e )
+				goexpr { while ( $t ) { $e } $gs ... }
     }
 
     rule {
-	{ if ( $t ) { $l .. } else { $r ... } $gs ... }
+				{ throw $e:expr; }
     } => {
-	(function () {
-	    var rest = function () { return goexpr { $gs ... } };
-	    return ( $t ) 
-		? ( goexpr { $l ... } ) . bind ( $rest ) 
-	        : ( goexpr { $r ... } ) . bind ( $rest );
-	}())
+				// monad (cont, fail) { return promise (fail ($e)) }
+				monad . fail ( $e )
     }
 
     rule {
-	try { $e ... } catch ($e:ident) { $f ... } finally { $f ... }
-	$gs ...
+				{ if ( $t ) { $l .. } else { $r ... } $gs ... }
     } => {
-	goexpr { $e ... }
+				(function () {
+						var rest = function () { return goexpr { $gs ... } };
+						return ( $t ) 
+								? ( goexpr { $l ... } ) . bind ( $rest ) 
+						: ( goexpr { $r ... } ) . bind ( $rest );
+				}())
     }
 
     rule {
-    	{ $g:expr ; }
+				try { $e ... } catch ($e:ident) { $f ... } finally { $f ... }
+				$gs ...
     } => {
-	monad.ret ($g);
+				goexpr { $e ... }
     }
 
     rule {
-	{ $g:expr ; $gs ... }
+    		{ $g:expr ; }
     } => {
-	monad.ret ( $g ) .bind ( function () { return goexpr { $gs ... } } )
+				monad.ret ($g);
     }
 
     rule {
-	{var $as ... ; $gs ... }
+				{ $g:expr ; $gs ... }
     } => {
-	(function () {
-	    var $as ... ;
-	    return goexpr { $gs ... };
-	}());
+				monad.ret ( $g ) .bind ( function () { return goexpr { $gs ... } } )
     }
 
     rule {
-	{$v:ident = $e:expr ; $gs ... }
+				{var $as ... ; $gs ... }
     } => {
-	(function () {
-	    $v = $e;
-	    return goexpr { $gs ... };
-	}());
+				(function () {
+						var $as ... ;
+						return goexpr { $gs ... };
+				}());
     }
 
     rule {
-	{}
+				{$v:ident = $e:expr ; $gs ... }
     } => {
-	monad.ret(undefined);
+				(function () {
+						$v = $e;
+						return goexpr { $gs ... };
+				}());
+    }
+
+    rule {
+				{}
+    } => {
+				monad.ret(undefined);
     }
 }
 
 macro go {
     rule {
-	{ $e ... }
+				{ $e ... }
     } => {
-	( goexpr { $e ... } ).run();
+				( goexpr { $e ... } ).run();
     }
 
     rule {
-	while ( $t:expr ) { $b ... }
+				while ( $t:expr ) { $b ... }
     } => {
-	go { while ( $t ) { $b ... } }
+				go { while ( $t ) { $b ... } }
     }
 
     rule {
-	while ( $t:expr ) $b:expr;
+				while ( $t:expr ) $b:expr;
     } => {
-	go while ( $t ) { $b }
+				go while ( $t ) { $b }
     }
 
     rule {
-	send $m:expr -> $ch:expr ;
+				send $m:expr -> $ch:expr ;
     } => {
-	go { send $m -> $ch; }
+				go { send $m -> $ch; }
     }
 }
 
