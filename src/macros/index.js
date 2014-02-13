42a4009a221765bd31bf05a14717f416145476a6
Your Name <frbracch@gmail.com>
2014-02-13 23:00:26 +0100

    changed comment style in macro file

--- src/macros/index.js
+++ src/macros/index.js
@@ -1,11 +1,9 @@
-/**
- * # Macro module
- *
- * This macro module is the core of the library, the exported macros
- * (`go { }` and `go_eval { }` ) walk through the body and rewrite
- * it according to the go semantic.
- */
+// This macro module is the core of the library, the exported macros
+// (`go { }` and `go_eval { }` ) walk through the body and rewrite
+// it according to the go semantic.
+///
 
+// core is a placeholder for the module
 macro core {
   rule {
   } => {
@@ -13,13 +11,12 @@ macro core {
   }
 }
 
-/**
- * ### gojs
- *
- * Wraps a javascript expression in a monad.
- * the js expression is wrapped in a function passed to  `_core_.ret'
- * because it can handle correctly exceptions raised by the js code.
- */
+// ### gojs
+//
+// Wraps a javascript expression in a monad.
+// the js expression is wrapped in a function passed to  `_core_.ret'
+// because it can handle correctly exceptions raised by the js code.
+//
 macro gojs {
   rule {
     ( $g ) { $e:expr }
@@ -43,11 +40,9 @@ macro gojs {
   }
 }
 
-/**
- * ### gofail
- *
- * Raise an exception
- */
+// ### gofail
+//
+// Raise an exception
 macro gofail {
   rule {
     ( $g ) $e:expr
@@ -56,11 +51,9 @@ macro gofail {
   }
 }
 
-/**
- * ### gobind
- *
- * Syntactic sugar used to simplify the call to the `Monad.bind` method.
- */
+// ### gobind
+//
+// Syntactic sugar used to simplify the call to the `Monad.bind` method.
 macro gobind {
   rule {
     $v:ident = $e:expr { $f:expr }
@@ -75,11 +68,9 @@ macro gobind {
   }
 }
 
-/**
- * ### goseq
- *
- * Syntactic sugar used to put 2 go_evalessions in sequence.
- */
+// ### goseq
+//
+// Syntactic sugar used to put 2 go_evalessions in sequence.
 macro goseq {
   rule infix {
     $e:expr | $f:expr
@@ -88,15 +79,13 @@ macro goseq {
   }
 }
 
-/**
- * ### gotry
- *
- * Synstactic sugar on try/catch.
- * TBD: why not finally (currently no because of the lack of continue/break)
- * example:
- *
- *     try <monad> catch (v) <monad>
- */
+// ### gotry
+//
+// Synstactic sugar on try/catch.
+// TBD: why not finally (currently no because of the lack of continue/break)
+// example:
+//
+//     try <monad> catch (v) <monad>
 macro gotry {
   rule {
     $e:expr catch ( $v:ident ) $f:expr
@@ -105,54 +94,52 @@ macro gotry {
   }
 }
 
-/**
- * # Go expression
- * This is the principal macro, it takes a list of statements, that are
- * javascript statements, augmented with a couple of new staments, namely
- * `send` statement and `recv` statement.
- *
- * a macro is by itself an expression, in particular an expression that
- * once evaluated generates an object of type Monad.
- *
- * the `go` macro is a wrapper arount `go_eval` that does more or less this:
- *
- *      go { $e ... } => ( go_eval { $e ... } ) . run()
- *
- * that is, call the method run on the monad.
- *
- * The nice things (I think) on this gozilla method are (see README.md for
- * more)
- *
- * 1. The various routines are not coupled, (i.e. a routine do not have to
- *   know the identity of the other routine providing data) because we
- *   are using channels, that are really data oriented synchronization
- *   structures.
- *
- * 1. Exceptions flow trasparently through the callbacks
- *
- * 1. The queueing mechanism permits more general control structures than
- *   the standard callback/promise/generatos
- *
- * 1. at the same time achieve the purpose of promises/generators of
- *   keeping the logic in a single place instead of scattered around.
- */
+// # Go expression
+// This is the principal macro, it takes a list of statements, that are
+// javascript statements, augmented with a couple of new staments, namely
+// `send` statement and `recv` statement.
+//
+// a macro is by itself an expression, in particular an expression that
+// once evaluated generates an object of type Monad.
+//
+// the `go` macro is a wrapper arount `go_eval` that does more or less this:
+//
+//      go { $e ... } => ( go_eval { $e ... } ) . run()
+//
+// that is, call the method run on the monad.
+//
+// The nice things (I think) on this gozilla method are (see README.md for
+// more)
+//
+// 1. The various routines are not coupled, (i.e. a routine do not have to
+//   know the identity of the other routine providing data) because we
+//   are using channels, that are really data oriented synchronization
+//   structures.
+//
+// 1. Exceptions flow trasparently through the callbacks
+//
+// 1. The queueing mechanism permits more general control structures than
+//   the standard callback/promise/generatos
+//
+// 1. at the same time achieve the purpose of promises/generators of
+//   keeping the logic in a single place instead of scattered around.
+///
 
 macro go_eval {
-  /**
-   * ## recv statement
-   *
-   *     recv val <- channel;
-   *
-   * when encountered, this statement tries to get a value from the channel
-   * `channel`, and put it in a newly created variable val. then continue
-   *
-   * IF the channel is empty, the routine suspend itself, and will be resumed
-   * when the channel will be able to provide new content.
-   *
-   * val should be an identifier, while channel any expression.
-   *
-   * The underlying logic is actually handled by the object `channel`.
-   */
+
+  // ## recv statement
+  //
+  //     recv val <- channel;
+  //
+  // when encountered, this statement tries to get a value from the channel
+  // `channel`, and put it in a newly created variable val. then continue
+  //
+  // IF the channel is empty, the routine suspend itself, and will be resumed
+  // when the channel will be able to provide new content.
+  //
+  // val should be an identifier, while channel any expression.
+  //
+  // The underlying logic is actually handled by the object `channel`.
 
   rule {
     ( $g ) { 
@@ -187,19 +174,18 @@ macro go_eval {
     gobind $v = $ch . recv () 
       => go_eval ( $g ) { $gs ... }
   }
-  /**
-   * ## send statement
-   *
-   *     send message -> channel;
-   *
-   * It's the counterpart of `recv`. When met tries to put a message in the
-   * channel. If the channel is buffered it can continue unless the buffer is
-   * full, otherwise it suspend itself.
-   *
-   * If the channel is unbuffered, the current thread is suspended. if some
-   * other threads are waiting for data, one of these is resumed, and once
-   * completed the current thread is resumed as well.
-   */
+
+  // ## send statement
+  //
+  //     send message -> channel;
+  //
+  // It's the counterpart of `recv`. When met tries to put a message in the
+  // channel. If the channel is buffered it can continue unless the buffer is
+  // full, otherwise it suspend itself.
+  //
+  // If the channel is unbuffered, the current thread is suspended. if some
+  // other threads are waiting for data, one of these is resumed, and once
+  // completed the current thread is resumed as well.
 
   rule {
     ( $g ) { 
@@ -218,19 +204,18 @@ macro go_eval {
     ( $ch ) . send ( $m ) goseq go_eval ( $g ) { $gs ... }
   }
 
-  /**
-   * ## while statement
-   *
-   *     while ($test) { $body ... }
-   *
-   * This statement tries to mimic javascript `while` loop.
-   * Since the body of the loop can obviously contain `recv` or `send` statements,
-   * it can't be implemented with javascript while, because these can't be resumed.
-   * the solution is to use a recursive function.
-   *
-   * The fact that it doesn't go in an infinite loop depends on the fact that
-   * the monad we are using provide a trampoline mechanism.
-   */
+  // ## while statement
+  //
+  //     while ($test) { $body ... }
+  //
+  // This statement tries to mimic javascript `while` loop.
+  // Since the body of the loop can obviously contain `recv` or `send` statements,
+  // it can't be implemented with javascript while, because these can't be resumed.
+  // the solution is to use a recursive function.
+  //
+  // The fact that it doesn't go in an infinite loop depends on the fact that
+  // the monad we are using provide a trampoline mechanism.
+
   rule {
     ( $g ) {
       while ( $t:expr ) { $b ... }
@@ -250,14 +235,14 @@ macro go_eval {
   } => {
     go_eval ( $g ) { while ( $t ) { $e } $gs ... }
   }
-  /**
-   * ## do while statement
-   *
-   *     do { $body } while ($test) ;
-   *
-   * this is pretty similar to the while loop, except that runs the body
-   * at least once.
-   */
+
+  // ## do while statement
+  //
+  //     do { $body } while ($test) ;
+  //
+  // this is pretty similar to the while loop, except that runs the body
+  // at least once.
+
   rule {
     ( $g ) {
       do { $b ... } while ( $t:expr ) ;
@@ -270,25 +255,23 @@ macro go_eval {
       } ) ; } () )
   }
 
-  /**
-   * ## return statement
-   *
-   * return statement in a routine does nothing. it evaluates the expression
-   * and nothing more, continue in any case.
-   * TODO: make this statement raise an error.
-   */
+  // ## return statement
+  //
+  // return statement in a routine does nothing. it evaluates the expression
+  // and nothing more, continue in any case.
+  // TODO: make this statement raise an error.
+
   rule {
     ( $g ) { return $e:expr ; }
   } => {
     gojs ( $g ) $e
   }
 
-  /**
-   * ## if statement
-   *
-   * As expected evaluates the test argument, if it is true, goes on the left,
-   * right otherwise.
-   */
+  // ## if statement
+  //
+  // As expected evaluates the test argument, if it is true, goes on the left,
+  // right otherwise.
+
   rule {
     ( $g ) { 
       if ( $t:expr ) { 
@@ -331,15 +314,14 @@ macro go_eval {
     go_eval ( $g ) { if ( $t ) $e ... else { $f } $gs ... }
   }
 
-  /**
-   * ## try/catch statement
-   *
-   * This is the try catch statement ;)
-   *
-   * The nice thing about this is that when a routine is suspended because of
-   * a channel op, when resumed, and the continuation raises an excpetion,
-   * this is raised to the external point.
-   */
+  // ## try/catch statement
+  //
+  // This is the try catch statement ;)
+  //
+  // The nice thing about this is that when a routine is suspended because of
+  // a channel op, when resumed, and the continuation raises an excpetion,
+  // this is raised to the external point.
+
   rule {
     ( $g ) { 
       try { $e ... } 
@@ -351,9 +333,8 @@ macro go_eval {
   } => {
     ( gotry go_eval ( $g ) { $e ... } catch ( $ex ) go_eval ( $g ) { $f ... } ) . bind ( function () { return go_eval ( $g ) { $gs ... } ; } )
   }
-  /**
-   * ## var's
-   */
+  // ## var's
+
   rule {
     ( $g ) { 
       var $( $a:ident = $e:expr ) (,) ...  ; 
@@ -374,11 +355,10 @@ macro go_eval {
     gojs ( $g ) { $v = $e; } goseq go_eval ( $g ) { $gs ... }
   }
 
-  /**
-   * ## general javascript expressions
-   *
-   * General javascript exceptions are evaluated as is.
-   */
+  // ## general javascript expressions
+  //
+  // General javascript exceptions are evaluated as is.
+
   rule {
     ( $g ) { $e:expr }
   } => {
@@ -390,12 +370,11 @@ macro go_eval {
   } => {
     gojs ( $g ) { $e }
   }
+  
+  // ## composition rule
+  //
+  // Main Composition rule: one expression, then the other :)
 
-  /**
-   * ## composition rule
-   *
-   * Main Composition rule: one expression, then the other :)
-   */
   rule {
     ( $g ) { 
       $g0:expr ; 
@@ -412,28 +391,27 @@ macro go_eval {
   }
 }
 
-/**
- * # Go macro
- *
- * This is the main way of building a goroutine, as told for go_eval,
- * the main scope of `go` is to wrap `go_eval` that actually build a
- * routine, and starting it.
- *
- *      go { $e ... } => ( go_eval { $e ... } ) . run()
- *
- * It provides some shortcuts for single statement expressions in
- * which the expression could be not wrapped in curly braces
- * i.e.
- *
- *      go while (test) { body } => go { while (test) { body } }
- *
- * #### value
- *
- * A go statement evaluates most probably to `"suspend"` if it is suspended 
- * on one of the `send` or `recv` operations.
- * this is not useful very for the final user, but gives an insight on
- * the way the engine works.
- */
+// # Go macro
+//
+// This is the main way of building a goroutine, as told for go_eval,
+// the main scope of `go` is to wrap `go_eval` that actually build a
+// routine, and starting it.
+//
+//      go { $e ... } => ( go_eval { $e ... } ) . run()
+//
+// It provides some shortcuts for single statement expressions in
+// which the expression could be not wrapped in curly braces
+// i.e.
+//
+//      go while (test) { body } => go { while (test) { body } }
+//
+// #### value
+//
+// A go statement evaluates most probably to `"suspend"` if it is suspended 
+// on one of the `send` or `recv` operations.
+// this is not useful very for the final user, but gives an insight on
+// the way the engine works.
+
 macro go {
   rule {
     { $e ... }
@@ -523,12 +501,11 @@ macro go {
   }
 }
 
-/**
- * # Export
- *
- * Exported macros are `go` and `go_eval`
- * the former the default case, the latter can be useful in case
- * the routine should be deferred, or used as a value.
- */
+// # Export
+//
+// Exported macros are `go` and `go_eval`
+// the former the default case, the latter can be useful in case
+// the routine should be deferred, or used as a value.
+
 export go;
 export go_eval;
